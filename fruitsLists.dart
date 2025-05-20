530780f7f91761253664246629c7b68aac22c521
Amjadyabroudi128 <61939508+Amjadyabroudi128@users.noreply.github.com>
2025-05-20 14:58:35 +0100

    Update fruitsLists.dart

--- fruitsLists.dart
+++ fruitsLists.dart
@@ -1,1 +1,12 @@
 // this is a list of fruits 
+void main() {
+  var fruits = [
+    "banana",
+    "strawberry",
+    "mango",
+    "orange",
+    "pomegranate",
+    "grapes"
+  ];
+  print(fruits);
+}
