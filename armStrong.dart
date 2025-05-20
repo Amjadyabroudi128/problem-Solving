b3cfff82d37e0566cff67651f97892ad124e5e5c
Amjadyabroudi128 <61939508+Amjadyabroudi128@users.noreply.github.com>
2025-05-20 14:03:55 +0100

    Create armStrong.dart

---
+++ armStrong.dart
@@ -0,0 +1,15 @@
+import 'dart:math';
+
+class ArmstrongNumbers {
+  bool isArmstrongNumber(String numberStr) {
+    BigInt numbers = BigInt.parse(numberStr); // convert string to int 
+    int numDigits = numberStr.length;
+    BigInt sum = BigInt.zero;
+
+    for (var digitChar in numberStr.split('')) {
+      sum += BigInt.from(int.parse(digitChar)).pow(numDigits); // Raise each digit to numDigits and add
+    }
+
+    return sum == numbers;
+  }
+}
