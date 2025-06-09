ddf06721cfa039502d29b86b97d2ee843275ee76
Amjadyabroudi128 <61939508+Amjadyabroudi128@users.noreply.github.com>
2025-06-09 15:58:00 +0100

    Create conver.dart

---
+++ conver.dart
@@ -0,0 +1,9 @@
+void main() {
+  // how to find Fahrenheit 
+  // to convert Celsius to Fahrenheit
+  // (Celsius * 9/5) + 32
+  double ceclius = 27;
+  double fahrenheit = ceclius * (9/5) + 32;
+  print (fahrenheit);
+  
+}
